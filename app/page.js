1a3c42f39c3d1feb460c6f5c7044d542a91dd0cc
Nicolas Nocete <nicolasnocete@gmail.com>
2024-02-22 14:37:59 -0300

    obligar a login para operar

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import { Skeleton, Box } from "@chakra-ui/react";
 import { useAuth } from "@/app/libs/AuthProvider";
 import ButtonCustom from "./componets/buttons/ButtonCustom";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Text } from "@chakra-ui/react";
 
 export default function Home() {
   const { loading, user } = useAuth();
@@ -13,6 +15,36 @@ export default function Home() {
   };
 
   if (loading) return <Skeleton height="100vh" />;
+
+  if (!user) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="50vh"
+        gap={8}
+      >
+        <Image
+          src={"/images/logo.svg"}
+          alt="Logitipo"
+          width="200"
+          height="100"
+        />
+        <Text
+          fontSize="2xl"
+          fontWeight="bold"
+          color="gray.500"
+          textAlign="center"
+        >
+          PIGO v2
+        </Text>
+        <Text>Inicie sesión para poder operar</Text>
+      </Box>
+    );
+  }
+
   return (
     <Box
       display="flex"
