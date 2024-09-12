1574adc0c83a8bd6688c9ea0b99c59b8c7956fd9
Agustin Galvan <agusstingalvan2@gmail.com>
2024-09-12 10:16:25 -0300

    redirect per function role

--- app/componets/Options.jsx
+++ app/componets/Options.jsx
@@ -2,6 +2,7 @@
 import { Box } from "@chakra-ui/react";
 import ButtonCustom from "./buttons/ButtonCustom";
 import { useAuth } from "@/app/libs/AuthProvider";
+import { useEffect } from "react";
 
 export default function Options({ router }) {
   const { user } = useAuth();
@@ -9,6 +10,27 @@ export default function Options({ router }) {
   const handleOption = (option) => {
     router.push(`/${option}`);
   };
+
+  useEffect(() => {
+    const allowedFunctions = [];
+    const {
+      permite_crear_fichada,
+      permite_crear_parte,
+      permite_crear_parte_sin_turno,
+      permite_crear_vale,
+      permite_crear_vale_sin_fichada,
+    } = user.role;
+
+    if (permite_crear_fichada) allowedFunctions.push("fichada");
+    if (permite_crear_parte || permite_crear_parte_sin_turno)
+      allowedFunctions.push("parte");
+    if (permite_crear_vale || permite_crear_vale_sin_fichada)
+      allowedFunctions.push("vale");
+
+    if (allowedFunctions.length === 1) {
+      router.push(`/${allowedFunctions[0]}`);
+    }
+  }, []);
 
   return (
     <Box
